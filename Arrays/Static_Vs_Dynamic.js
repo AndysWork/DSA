1c5352d3649a26fc758451475cf286c787da7ba9
Gourab <smlgourab@gmail.com>
2024-08-03 23:24:57 +0530

    Static_V_Dynamic

---
+++ Arrays/Static_Vs_Dynamic.js
@@ -0,0 +1,14 @@
+/* 
+Static Arrays: Most of the programming languages have static arrays. The size of the array is fixed and 
+cannot be changed during the execution of the program. The size of the array is determined at the time of declaration.
+
+int a[5]; // Static array of size 5
+
+Dynamic Arrays: Dynamic arrays are arrays that can grow or shrink in size during the execution of the program.
+In dynamic arrays, the size of the array is not fixed and can be changed during the execution of the program.
+
+const strings = ["a", "b", "c", "d"]; - size is 4
+strings.push("e"); - size is 5 (dynamic array)
+
+For Dynamic Arrays, append operation is O(1) on average, but it can be O(n) in the worst case.
+*/

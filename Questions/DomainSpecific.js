8f54579f0c64480cf66aabfe1cf3378d2857d8df
Gourab <smlgourab@gmail.com>
2024-08-12 02:47:44 +0530

    Questions Commit

---
+++ Questions/DomainSpecific.js
@@ -0,0 +1,21 @@
+/*
+Domain Specific Questions
+With smaller companies, you may encounter more domain specific questions when it comes to coding interviews.
+What does this mean? It means that the questions focus less on computer science fundamentals like 
+Data Structures and Algorithms, and instead they focus on the technology stack that the company is 
+actively working with. To practice these questions, I have listed a great list for you to go through 
+based on what type of job you are applying to (React developer, Mobile developer, etc...):
+
+https://github.com/MaximAbramchuck/awesome-interview-questions
+
+Also:
+
+Front End Developer: https://github.com/h5bp/Front-end-Developer-Interview-Questions
+
+Javascript Interview Questions: 
+1 - https://www.toptal.com/javascript/interview-questions
+2 -
+3 - https://www.thatjsdude.com/interview/js2.html
+
+PS, keep in mind that it will be hard to just get everything right from the beginning.
+*/
